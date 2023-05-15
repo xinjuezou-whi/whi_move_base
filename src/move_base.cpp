de77e7f5d965191930b1b0efbce53b578b540ebb
Xinjue Zou <hit_zou@126.com>
2023-05-15 19:49:15 +0800

    revise interaction flag to message mechanism to resolve frame_id transform issue

--- src/move_base.cpp
+++ src/move_base.cpp
@@ -287,6 +287,12 @@ namespace move_base {
   }
 
   void MoveBase::goalCB(const geometry_msgs::PoseStamped::ConstPtr& goal){
+    if (int_state_ == INT_BLOCKED)
+    {
+      ROS_DEBUG_NAMED("move_base", "Aborting on goal for being occupied by interacting action");
+      return;
+    }
+
     ROS_DEBUG_NAMED("move_base","In ROS goal callback, wrapping the PoseStamped in the action message and re-sending to the server.");
     move_base_msgs::MoveBaseActionGoal action_goal;
     action_goal.header.stamp = ros::Time::now();
